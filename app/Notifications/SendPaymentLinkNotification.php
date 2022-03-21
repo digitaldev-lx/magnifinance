7a10e1165b6a455aa9b94b6baf24152215ef6fd6
Paulo Serrano <geral@digitaldev.pt>
2022-03-21 15:21:11 +0000

    debug pos booking

--- app/Notifications/SendPaymentLinkNotification.php
+++ app/Notifications/SendPaymentLinkNotification.php
@@ -50,7 +50,7 @@ class SendPaymentLinkNotification extends BaseNotification implements ShouldQueu
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->from([config("mail.from.address"), config("mail.from.name")])
+            ->from([config("mail.from.name"), config("mail.from.address")])
             ->greeting(__("email.hello") . " " . $notifiable->name)
             ->line(__('email.thankForYourContactAndInterest'))
             ->line(__('email.confirmAppointmentPaymentButton'))
